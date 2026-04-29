e1b73cedef5c726124fa8ab5fe23a05065352a0d
tosif121 <tosifraza8867@gmail.com>
2026-04-29 13:23:51 +0530

    fix: mark metadata routes as force-static for static export

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,6 @@
 import { MetadataRoute } from 'next';
+
+export const dynamic = 'force-static';
 import { BLOG_POSTS } from '@/lib/blog-posts';
 
 export default function sitemap(): MetadataRoute.Sitemap {
